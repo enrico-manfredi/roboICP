0eb91d749c9ab0c7e9e1db951b30bbb0868ce663
enrico-manfredi <30388890+enrico-manfredi@users.noreply.github.com>
2021-09-28 18:36:54 +0100

    Update actuatorTest.py
    -added query pointing to import
    -captialised class to match changes to imported script

--- Code/actuatorTest.py
+++ Code/actuatorTest.py
@@ -1,8 +1,11 @@
 from actuatorLib import *
 
-act = actuator()
+act = Actuator()
 
 act.forward(5)
 act.backward(5)
 act.forward(7)
 act.backward(7)
+
+##could this be put into the test script inside actuatorLib.py?
+##see query in lines 20 & 21
