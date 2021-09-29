c27b3e4724da71195f6f9aa51ab610dfab5db752
enrico-manfredi <30388890+enrico-manfredi@users.noreply.github.com>
2021-09-29 09:32:44 +0100

    Update linearRailLib.py
    -added comments
    -added docstrings
    -capitalised classname
    -added queries

--- Code/linearRailLib.py
+++ Code/linearRailLib.py
@@ -1,63 +1,82 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-class linearRail:
+class LinearRail:
 
     def __init__(self):
         
-        self.direction = 11
-        self.pulse = 13
+        self.direction = 11 #gpio pin that determines direction of slide movement
+        self.pulse = 13 #gpio pin that moves the slide when high
 
         GPIO.setup(self.direction, GPIO.OUT)
         GPIO.setup(self.pulse, GPIO.OUT)
 
-        GPIO.output(self.direction,GPIO.HIGH)
+        GPIO.output(self.direction,GPIO.HIGH) #default direction is forward
 
         self.position = 0
-        self.steps_per_syringe = 50000
-        self.first_pos = 3600 * 5
+        
+        ##why is position set to zero before self.calibrate is called?
+        
+        self.steps_per_syringe = 50000 #note: steps are absolute not relative
+        self.first_pos = 3600 * 5 ##what does this refer to? first syringe pos?
+        
+        ##how long is each step in mm?
 
         self.calibrate()
 
     def calibrate(self):
         self.backward()
         self.move(200000)
+        
+        ##there is no backrun protection for the linear rail so it grinds itself out
+        ##need to add a limit switch instead of just having it bash on the end
+        ##probably some additional circuitry required to add an interrupt signal
 
     def backward(self):
-        GPIO.output(self.direction,GPIO.LOW)
+        GPIO.output(self.direction,GPIO.LOW) #backward is low
     
     def forward(self):
-        GPIO.output(self.direction,GPIO.HIGH)
+        GPIO.output(self.direction,GPIO.HIGH) #forward is high
 
     def moveToSyringe(self,n_syringe):
+        """enter syringe # to dispense between 0-4 with 0 being the first"""
+        #self.firstpos is the first syringe and takes care of n=0 case
         syringe_step_pos = (self.steps_per_syringe * n_syringe) + (self.first_pos)
         steps_to_move = syringe_step_pos - self.position
         self.moveToPos(steps_to_move)
 
     def moveToPos(self,pos):
+        """moves rail to specified position in steps (int)"""
+        #if the number of steps to targ is less than current pos, go back
         if pos < self.position:
             self.backward()
+        #if the number of steps to targ is greater than current pos, go forward
         else: 
             self.forward()
 
-        self.move(abs(pos-self.position))
+        self.move(abs(pos-self.position)) #determines how far to move by taking absolute diff
+        #direction to move is set above
 
-        self.position = pos
+        self.position = pos #updates position with targ once move is finished
         
     def move(self,n_of_pulses):
+        """moves stepper by one pulse (step)"""
         for i in range(n_of_pulses):
             GPIO.output(self.pulse,GPIO.HIGH)
             GPIO.output(self.pulse,GPIO.LOW)
-            sleep(0.001)
+            sleep(0.001) #reset time for next pulse
 
     def reset(self):
+        """switches to backwards direction and moves\
+        same number of steps back as current position"""
         GPIO.output(self.direction,GPIO.LOW)
         self.move(self.position)
         GPIO.output(self.direction,GPIO.HIGH)
 
+#unit test - silent on import
 if __name__ == '__main__':
     GPIO.setmode(GPIO.BOARD)
-    li = linearRail()
+    li = LinearRail()
     sleep(5)
     for syringe in range(5):
         print("yee")
